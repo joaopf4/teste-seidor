9057e5a8f601f4ed5c921fdff58ac94c3418e691
Joao Pedro <joaopfa@hotmail.com>
2021-03-23 22:41:53 -0300

    form fields first steps

---
+++ src/theme/index.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,500&display=swap');
+  body {
+    margin: 0;
+    padding:0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-family: 'Montserrat', sans-serif;
+    overflow-y: overlay;
+    width: 100vw; 
+    padding-left: 0px !important;
+    }
+
+  * {
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+`;
+export const theme = {
+    white: "#fffffb",
+    darkBlue: "#185785",
+    orange: "#f3a52e",
+    lightBlue: "#dde3e6",
+    lightOrange: "#f3d7b6",
+    black:"#000000",
+    creamWhite: "#f2f2f2"
+}
